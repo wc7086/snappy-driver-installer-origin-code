719405f5307ee133099769d5e9d3e828459d4a4f
joodleberry <joodleberry@a35f1cdb-9e8a-4e40-bfb5-046fbceb7d32>
2016-10-30 01:52:47 +0000

    Added Thai language

--- trunk/tools/SDI/langs/move_all.bat
+++ trunk/tools/SDI/langs/move_all.bat
@@ -31,3 +31,23 @@ copy gui_no.ini norwegian.txt
 copy gui_ka.ini georgian.txt
 copy gui_da.ini danish.txt
 copy gui_ko.ini korean.txt
+copy gui_th.ini thai.txt
+
+rem following exist in transifex but currently not supported in SDI
+rem copy gui_bn.ini bengali.txt
+rem copy gui_bs.ini bosnian.txt
+rem copy gui_hi.ini hindi.txt
+rem copy gui_mk.ini macedonian.txt
+rem copy gui_si.ini sinhala.txt
+rem copy gui_zh_TW.ini chinese_tw.txt
+rem copy gui_sq.ini albanian.txt
+rem copy gui_ms.ini malay.txt
+rem copy gui_sk.ini slovak.txt
+rem copy gui_sr.ini serbian.txt
+rem copy gui_fi.ini finnish.txt
+rem copy gui_id.ini indonesian.txt
+rem copy gui_zh.ini chinese.txt
+rem copy gui_sl.ini slovenian.txt
+rem copy gui_ca.ini catalan.txt
+rem copy gui_hr.ini croatian.txt
+rem copy gui_en.ini english.txt
