22c850d144ded9526e7aa1df1e37a03a5fa49718
joodleberry <joodleberry@a35f1cdb-9e8a-4e40-bfb5-046fbceb7d32>
2017-03-10 09:16:24 +0000

    Add Origin to title, fix version resource

--- source/svnrev_template.h
+++ source/svnrev_template.h
@@ -25,8 +25,8 @@ along with Snappy Driver Installer.  If not, see <http://www.gnu.org/licenses/>.
 #define SVN_REV_Y $WCDATE=%Y$
 
 #if $WCMODS?1:0$
-#define SVN_REV_STR "R$WCREV$[Modified] ($WCNOW=%d.%m.%Y$)"
-#define SVN_REV2    "R$WCREV$[Modified]"
+#define SVN_REV_STR "R$WCREV$ [Modified] ($WCNOW=%d.%m.%Y$)"
+#define SVN_REV2    "R$WCREV$ [Modified]"
 #else
 #define SVN_REV_STR "R$WCREV$ ($WCDATE=%d.%m.%Y$)"
 #define SVN_REV2    "R$WCREV$"
